774caa908116923579d51f7e95d3a56c39c2e4fa
Martin-J-Larre <martinjlarre@gmail.com>
2022-03-22 00:35:29 -0400

    #3 bug deno with servest all flies

--- server.tsx
+++ server.tsx
@@ -2,11 +2,32 @@
 import React from "https://dev.jspm.io/react/index.js";
 // @deno-types="https://deno.land/x/servest@v1.3.1/types/react-dom/server/index.d.ts"
 import ReactDOMServer from "https://dev.jspm.io/react-dom/server.js";
-import { createApp } from "https://deno.land/x/servest@v1.3.1/mod.ts";
+import {
+    contentTypeFilter,
+    createApp,
+} from "https://deno.land/x/servest@v1.3.1/mod.ts";
 
 const app = createApp();
-let visitas: number = 0
+let colors: Array<any> = [];
 
+const listColors = colors.map((color) => (<li>color</li>));
+
+const htmlPage = (
+    <html>
+        <head>
+            <meta charSet="utf-8" />
+            <title>Servest</title>
+        </head>
+        <body>
+            <h1>Pick the color</h1>
+            <form action="/color" method="post">
+                <input type="color" name="color"></input>
+                <input type="submit"></input>
+            </form>
+            <ul>{colors}</ul>
+        </body>
+    </html>
+);
 
 app.handle("/", async (req) => {
     await req.respond({
@@ -14,20 +35,28 @@ app.handle("/", async (req) => {
         headers: new Headers({
             "content-type": "text/html; charset=UTF-8",
         }),
-        body: ReactDOMServer.renderToString(
-            <html>
-                <head>
-                    <meta charSet="utf-8" />
-                    <title>Servest</title>
-                </head>
-                <body>
-                    <h1 style={{color:'blue'}}>Hello Servest con React!!!!</h1>
-                    <h2 style={{color:'red'}}>Visaitas:{++visitas}</h2>
-                    <h3 style={{color:'orange'}}>FyH: {new Date().toLocaleString()}</h3>
-                </body>
-            </html>
-        ),
+        body: ReactDOMServer.renderToString(htmlPage),
     });
 });
 
-app.listen({ port: 8899 });
+app.post(
+    "/color",
+    contentTypeFilter("application/x-www-form-urlencoded"),
+    async (req) => {
+        const bodyForm = await req.formData();
+        const color = bodyForm.value("color");
+        colors.push(color);
+        await req.respond({
+            status: 200,
+            headers: new Headers({
+                "content-type": "text/html; charset=UTF-8",
+            }),
+            body: ReactDOMServer.renderToString(htmlPage),
+        });
+    }
+);
+
+app.listen({ port: 8080 });
+
+//! Cambiar a versión 1.8 de Deno para que ande ya que la última versión no tiene solucionado el bug
+//! https://stackoverflow.com/questions/71330388/cant-run-server-with-deno-and-servest
